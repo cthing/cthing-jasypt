bd14b87d47a6b9784ace52a3a0cbc64dd6950cc9
Baron Roberts <baron@cthing.com>
2021-08-09 00:42:23 -0700

    Upgrade Gradle and dependencies.

--- build.gradle.kts
+++ build.gradle.kts
@@ -31,7 +31,7 @@ dependencies {
 
     testImplementation("org.junit.jupiter:junit-jupiter-api:5.7.2")
     testImplementation("org.junit.jupiter:junit-jupiter-params:5.7.2")
-    testImplementation("org.assertj:assertj-core:3.20.0")
+    testImplementation("org.assertj:assertj-core:3.20.2")
 
     testRuntimeOnly("org.junit.jupiter:junit-jupiter-engine:5.7.2")
 }
