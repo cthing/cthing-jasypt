6832330cb8b7689fd214da5fb2b4aaa22e9f10d7
Baron Roberts <baron@cthing.com>
2025-02-07 01:34:46 -0800

    Upgrade Gradle and dependencies

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -1,3 +1,10 @@
 apply(from = File(gradle.gradleUserHomeDir, "cthing-settings.gradle.kts"))
 
 rootProject.name = "cthing-jasypt"
+
+pluginManagement {
+    repositories {
+        mavenLocal()
+        gradlePluginPortal()
+    }
+}
